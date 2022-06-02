426b54e3424a61b3238bf9f299cda6058d4e0046
LirazGanon <105818149+LirazGanon@users.noreply.github.com>
2022-06-02 09:03:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+//check for Navigation Timing API support
+if (window.performance) {
+  console.info("window.performance works fine on this browser");
+  document.querySelector(".reload-img").setAttribute("onclick", "onClickNuni()");
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+console.info(performance.navigation.type);
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+  console.info("This page is reloaded");
+  document.querySelector(".reload-img").setAttribute("onclick", "location.reload()");
+// Change the dice image
+  var randomNumber1 = Math.ceil(Math.random() * 6); //1-6
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");//change Player1's dice image src
+  var randomNumber2 = Math.ceil(Math.random() * 6); //1-6
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");//change Player2's dice image src
+
+// Change 'h1' and 'p'w' depend on the winning player
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+    document.querySelectorAll("p")[0].innerHTML = "<strong>player1</strong>"
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
+    document.querySelectorAll("p")[1].innerHTML = "<strong>player2</strong>"
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!"
+  }
+
+
+} else {
+  console.info("This page is not reloaded");
 }
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
+
+function onClickNuni(){
+  alert("תהני מהמשחק רובא!");
+  location.reload();
 }
